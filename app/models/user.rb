3f8dcf29d5b08fee684d57cdf92ebba7d202d8ff
Rich Donnellan <rich.donnellan@gmail.com>
2015-11-03 17:29:58 -0800

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  before_save { email.downcase! }
+  before_save { fname.capitalize! }
+  before_save { lname.capitalize! }
+  validates :fname,  presence: true, length: { maximum: 25 }
+  validates :lname,  presence: true, length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
