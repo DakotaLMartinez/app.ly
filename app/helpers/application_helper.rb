f49dfaa8ecd8f075e6157ed666cc766e78d0214b
Rich Donnellan <rich.donnellan@gmail.com>
2015-11-04 00:47:02 -0800

    Add application helper for page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return full title on a per-page basis
+  def full_title(page_title = '')
+    base_title = "App.ly - The documenting tool for every job seeker"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} &0124; #{base_title}"
+    end
+  end
 end
